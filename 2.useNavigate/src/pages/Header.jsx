9aae3b244a8a243d67077d41d0d51dd5e0399e15
Tayfun çakır <tayfun.cakirr9@gmail.com>
2025-08-20 11:02:28 +0300

    usenavigate

--- 2.useNavigate/src/pages/Header.jsx
+++ 2.useNavigate/src/pages/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+//  useNavigate, link bileşenine benzer, Sayfalar arasında gezinmeyi sağlar.
+
+
 
 function Header() {
     const navigate = useNavigate();
   return (
-    <div>
+    <div className='header'>
         <ul>
             <li onClick={()=> navigate("/Menu")} >Menu</li>
             <li onClick={()=> navigate("/product")} >Product</li>
